fc6fcd60c684ef03aca1794a502978679ae5a358
aya <ayaslay19@gmail.com>
2022-08-04 23:05:19 -0600

    Added variable in same file

--- _main.tf
+++ _main.tf
@@ -6,7 +6,7 @@ resource  "aws_instance" "webserver" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.webserver_port} &
                 EOF
     
     user_data_replace_on_change = true
@@ -22,13 +22,18 @@ resource "aws_security_group" "webserver-sg" {
     name            = "simple-webserver-sg"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.webserver_port
+        to_port     = var.webserver_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
+variable "webserver_port" {
+    description = "The port the server will use for HTTP requests"
+    type        = number
+    default     = 8080
+}
 
 # amazon linux ami-090fa75af13c156b4
 # ubuntu 20.04 ami-08d4ac5b634553e16
